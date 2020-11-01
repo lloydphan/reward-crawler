eb1df98b0e4d83ef9f0779de626d5df77dd620d0
Tu Phan <tuantu.phan@cimb.com>
2020-11-02 01:16:04 +0800

    Update Data Crawler

---
+++ rewards/file.py
@@ -0,0 +1,37 @@
+import os
+
+
+class File:
+    file = ""
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def isFile(self, file_name):
+        if os.path.isfile(file_name):
+            return True
+        else:
+            return False
+
+    def openOrCreate(self):
+        if self.isFile(self.file_name):
+            with open(self.file_name, "r") as file:
+                return file
+        else:
+            with open(self.file_name, "w+") as file:
+                return file
+
+    def readFile(self):
+        with open(self.file_name, "r") as reader:
+            return reader.read()
+
+    def readLnsFile(self):
+        with open(self.file_name, "r") as reader:
+            return reader.readlines()
+
+    def writeFile(self, line):
+        with open(self.file_name, "a") as wr:
+            wr.write(line + "\n")
+
+    def rename(self, old_name, new_name):
+        os.rename(old_name, new_name)
---
+++ rewards/spiders/rewards_spider.py
@@ -0,0 +1,60 @@
+import scrapy
+import logging
+import os
+from rewards.file import File
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "rewards"
+    total_record = 319
+    domains = "https://rewards.cimbbank.com.my"
+    results_updated = "results_updated.txt"
+    results = "results.txt"
+
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.update_file = File(self.results_updated)
+        if self.update_file.isFile(self.results_updated) is False:
+            self.new_file = File(self.results)
+            self.new_file.openOrCreate()
+        else:
+            logging.debug("Haha:" + str(self.update_file.readLnsFile()))
+
+    def start_requests(self):
+        logging.debug("bbb" + str(self.update_file))
+        if self.new_file:
+            logging.debug(self.update_file)
+            str_exp = 'https://rewards.cimbbank.com.my/index.php?ch=cb_rwd&pg=cb_rwd_itms&nhmxl5=10&stmxl5='
+            urls = self.getArrayUrls(self.total_record, str_exp)
+            for url in urls:
+                yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        if self.new_file:
+            for row in response.xpath('//tr[has-class("ctlgLine")]')[1:]:
+                code = str(row.xpath('td[1]//text()').extract_first()).strip()
+                if self.isNotBlank(code):
+                    image = str(self.domains + "/" + str(row.xpath('td[1]//a//img/@src').extract_first()))
+                    desc = str(row.xpath('td[2]//node()').extract_first())
+                    price = str(row.xpath('td[3]//text()').extract_first()).replace('BP', '').strip()
+                    line = "code:" + code + ";image:" + image + ";desc:" + desc + ";price:" + price
+                self.new_file.writeFile(line)
+
+    def __del__(self):
+        if self.new_file:
+            self.new_file.rename(self.results, self.results_updated)
+            logging.debug("Ket thuc")
+
+    def getArrayUrls(self, total_record, str_exp):
+        urls = []
+        number_page = round(total_record / 10)
+        for i in range(number_page):
+            url = str_exp + str(i * 10)
+            urls.append(url)
+        return urls
+
+    def isNotBlank(self, string):
+        return bool(string and string.strip())
+
+    def rename(self, old_name, new_name):
+        os.rename(old_name, new_name)
